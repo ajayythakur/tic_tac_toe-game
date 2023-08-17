b35409baf6acc341912b246b7955b3ff9e496a3f
ajayythakur <ajay1270panwar@gmail.com>
2023-08-17 17:04:08 +0530

    Tic-Tac-Toe

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ function App() {
     <div>
       <h1 className='title'>Welcome to Tic-Tac-Toe</h1>
       <Board/>
+      <p>Point to remember</p>
+      <ul>
+      <li>Player 1 will be assigned X</li>
+      <li>Player 2 will be assigned O</li>
+
+      </ul>
     </div>
   );
 }
